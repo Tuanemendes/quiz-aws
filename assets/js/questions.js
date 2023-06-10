67b4ac3c3530ccdb656b853ee45b340e48a04558
Tuanemendes <tuane.belmiro@gmail.com>
2023-06-10 01:39:53 -0300

    criação da logica do quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+const questions = [
+    {
+        question: "Qual é o principal benefício da implantação de um banco de dados RDS em uma configuraçãoMulti-AZ? ",
+        options: ["Aumentar a disponibilidade do banco de dados",
+            "Proteger o banco de dados de um falharegional",
+            "Reduzir os custos de uso do banco de dados",
+            "Melhorar o desempenho do banco de dados para cargas de trabalho pesadas prontas"],
+        answer: 0
+    }
+
+]
+
+export default questions;
---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+import quizData from './quizData.js';
